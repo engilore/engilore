b5b02b3c47524e2fc4a4372fb272f6fdb559cb73
Lalo <lalo@physicslab.space>
2024-09-13 00:04:31 -0500

    adding slug updating

--- apps/category/models.py
+++ apps/category/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.text import slugify
-
 from account.models import User
 
 
@@ -11,69 +10,79 @@ class Category(models.Model):
         null=True,
         blank=True,
         related_name='created_categories',
-        )
+    )
     name = models.CharField(
         max_length=100,
         unique=True,
         null=False,
         blank=False
-        )
+    )
     description = models.TextField(
         null=True,
         blank=True
-        )
+    )
     slug = models.SlugField(
         max_length=255,
         unique=True,
         null=False,
         blank=True
-        )
+    )
     meta_title = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        )
+    )
     meta_description = models.CharField(
         max_length=300,
         null=True,
         blank=True,
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         null=False,
         blank=False
-        )
+    )
     updated_at = models.DateTimeField(
         auto_now=True,
         null=False,
         blank=False
-        )
+    )
 
-    def _generate_slug(self):
-        if not self.slug:
-            original_slug = slugify(self.name)
-            slug = original_slug
-            count = 1
+    def _generate_unique_slug(self):
+        original_slug = slugify(self.name)
+        slug = original_slug
+        count = 1
 
-            while Category.objects.filter(slug=slug).exists():
-                slug = f'{original_slug}-{count}'
-                count += 1
+        while Category.objects.filter(slug=slug).exists():
+            slug = f'{original_slug}-{count}'
+            count += 1
 
-            self.slug = slug
+        return slug
 
-    def _generate_meta_title(self):
+    def _set_slug(self):
+        if not self.slug or self._is_name_changed():
+            self.slug = self._generate_unique_slug()
+
+    def _is_name_changed(self):
+        if self.pk:
+            existing_name = Category.objects.filter(pk=self.pk).values_list('name', flat=True).first()
+            return existing_name != self.name
+        return False
+
+    def _set_meta_title(self):
         if not self.meta_title:
             self.meta_title = self.name
 
-    def _generate_meta_description(self):
+    def _set_meta_description(self):
         if not self.meta_description:
             description_source = self.description or self.name
             self.meta_description = description_source[:155] + '...' if len(description_source) > 155 else description_source
 
     def save(self, *args, **kwargs):
-        self._generate_slug()
-        self._generate_meta_title()
-        self._generate_meta_description()
+        self._set_slug()
+        self._set_meta_title()
+        self._set_meta_description()
+
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -86,7 +95,6 @@ class Category(models.Model):
             models.Index(fields=['slug']),
             models.Index(fields=['name']),
         ]
-
 
 
 class Topic(models.Model):
