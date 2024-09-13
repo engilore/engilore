79f331f5109d13ed3dd449b1f5bfd04f39317db3
Lalo <lalo@physicslab.space>
2024-09-12 21:11:37 -0500

    fixing guardian assignment

--- apps/account/checkers.py
+++ apps/account/checkers.py
@@ -1,9 +1,11 @@
 
 
 def check_if_admin_or_guardian(self):
-        if self.email == self.ADMIN_EMAIL:
-            self.is_admin = True
-            self.is_guardian = True
-        else:
+    if self.email == self.ADMIN_EMAIL:
+        self.is_admin = True
+        self.is_guardian = True
+    else:
+        if not self.is_admin:
             self.is_admin = False
+        if not self.is_guardian:
             self.is_guardian = False
