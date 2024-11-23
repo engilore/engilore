ef4d5867a92f02bfc604ce99eaa28e7e385f0064
Lalo Blanc <lalo@physicslab.space>
2024-11-23 10:58:09 -0800

    core config

---
+++ apps/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from blog.models import BlogPost
+
+
+def core_home(request):
+    spotlighted_posts = BlogPost.objects.filter(is_spotlighted=True, status='published')[:3]
+    return render(request, 'core_templates/views/core_home.html', {'spotlighted_posts': spotlighted_posts})
+
+def core_about(request):
+    return render(request, 'core_templates/views/core_about.html', {'title': 'About'})
