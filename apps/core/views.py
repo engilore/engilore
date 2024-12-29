edc307603f754aa7995311947e1b411b80bc5f69
Lalo Blanc <lalo@physicslab.space>
2024-12-28 20:44:46 -0800

    recent posts implementation

--- apps/core/views.py
+++ apps/core/views.py
@@ -10,14 +10,20 @@ def core_home(request):
         status='published',
         published_at__isnull=False
     ).order_by('-published_at')[:3]
-    
+
+    recent_posts = BlogPost.objects.filter(
+        is_spotlighted=False,
+        status='published',
+        published_at__isnull=False
+    ).order_by('-published_at')[:4]
+
     projects = Project.objects.all().order_by('-created_at') 
 
     return render(request, 'core_templates/views/core_home.html', {
         'spotlighted_posts': spotlighted_posts,
+        'recent_posts': recent_posts,
         'projects': projects,
     })
-
 
 def core_about(request):
     return render(request, 'core_templates/views/core_about.html')
